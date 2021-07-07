4f5f2bff0921104c2ab63472e521dcf0c2ca8fcd
Aziyza <aziza.ismailova.99@gmail.com>
2021-07-08 03:08:04 +0500

    UI part is added

---
+++ Book_Searcher/View/BookCell.swift
@@ -0,0 +1,17 @@
+//
+//  BookCell.swift
+//  Book_Searcher
+//
+//  Created by Mac on 7/8/21.
+//
+
+import UIKit
+
+class BookCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
